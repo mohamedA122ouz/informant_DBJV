30c5008e12f981910d9df5c9ad39a4d868d0ce50
mohameda122ouz <studyatmohamed@gmail.com>
2024-05-01 17:59:00 +0300

    adding loging-out and fixing a bug with loging-out

--- src/informantdb/InformantDB.java
+++ src/informantdb/InformantDB.java
@@ -25,17 +25,19 @@ public class InformantDB {
     static protected EntityManagerFactory factory = Persistence.createEntityManagerFactory("informantDBPU");
     static protected EntityManager manager = factory.createEntityManager();
     static protected EntityTransaction transaction = manager.getTransaction();
-    public static String listAds(){
-        Query q = manager.createNamedQuery("Ads.findAll",Ads.class);
-       List<Ads> ads = (List<Ads>) q.getResultList();
-       String list = "[\n";
-       for(var i : ads){
-           list += ads +"\n";
-       }
-       list += "\n]";
+
+    public static String listAds() {
+        Query q = manager.createNamedQuery("Ads.findAll", Ads.class);
+        List<Ads> ads = (List<Ads>) q.getResultList();
+        String list = "[\n";
+        for (var i : ads) {
+            list += ads + "\n";
+        }
+        list += "\n]";
         return list;
     }
-    static public String SignIn(UserInput user, String token) throws InterruptedException {
+
+    static public String SignIn(UserInput user) throws InterruptedException {
         try {
             if (user != null) {
                 Query q = manager.createNamedQuery("User.findByName", User.class);
@@ -46,6 +48,7 @@ public class InformantDB {
                     return "Error::NOt found";
                 } else if (addedUser.currentUser.getPassword().equals(user.Password)) {
                     String userToken = TokenProducer.produce();
+                    String token = addedUser.currentUser.getToken();
                     users.put(userToken, addedUser); //defualt one
                     if (token != null) {
                         try {
@@ -71,9 +74,22 @@ public class InformantDB {
                     System.out.println("New Token: " + userToken);
                     return userToken;
                 }
-            } else if (token != null) {
-                Query q = manager.createNamedQuery("User.findByToken");
-                var currentUser = new User();
+            }
+            System.out.println("BAD REQUEST NO PARAMETERS PROVIDED OR FAILD TO SING-IN");
+            return "Error::BAD REQUEST NO PARAMETERS PROVIDED";
+        } catch (Exception ex) {
+            System.out.println("BAD REQUEST " + ex.getMessage());
+            return "Error::BAD REQUEST " + ex.getMessage();
+        }
+    }
+
+    static public String tokenSignIn(String token) throws InterruptedException {
+        try {
+
+            if (token != null) {
+                Query q = manager.createNamedQuery("User.findByToken", User.class);
+                q.setParameter("token", token);
+                User currentUser = null;
                 if (!users.isEmpty()) {
                     try {
                         if (users.get(token) != null) {
@@ -109,12 +125,11 @@ public class InformantDB {
                     return userToken;
                 }
             }
-            System.out.println("BAD REQUEST NO PARAMETERS PROVIDED OR FAILD TO SING-IN");
-            return "Error::BAD REQUEST NO PARAMETERS PROVIDED";
         } catch (Exception ex) {
-            System.out.println("BAD REQUEST " + ex.getMessage());
-            return "Error::BAD REQUEST " + ex.getMessage();
+            System.out.println("Error::" + ex.getMessage());
+            return "Error::" + ex.getMessage();
         }
+        return "Error::TOken not exist";
     }
 
     static public void createUser(createUser user1) {
@@ -132,63 +147,66 @@ public class InformantDB {
     static HashMap<String, userBaseData> users = new HashMap<String, userBaseData>();
     static ArrayList<Ads> adList = new ArrayList<Ads>();
 
+    static public void changeData() throws InterruptedException {
+        Scanner u = new Scanner(System.in);
+        System.out.println("Please sign in first: ");
+        System.out.print("Enter UserName: ");
+        String username = u.nextLine();
+        System.out.print("Enter Password: ");
+        String password = u.nextLine();
+        var signINUser = new UserInput(username, password);
+        var token = SignIn(signINUser);
+        if (!token.contains("Error::")) {
+            User signedInUser = users.get(token).currentUser;
+            System.out.println("DETAILS :: " + signedInUser);
+            System.out.print("Enter what you data you change: ");
+            String tempS = u.nextLine();
+            transaction.begin();
+            if (tempS.toLowerCase().contains("email")) {
+                System.out.print("Email: ");
+                String t = u.nextLine();
+                signedInUser.setEmail(t);
+            } else if (tempS.toLowerCase().contains("password")) {
+                System.out.print("password: ");
+                String t = u.nextLine();
+                signedInUser.setPassword(t);
+            } else if (tempS.toLowerCase().contains("name")) {
+                createUser createdUser = new createUser();
+                Query q = manager.createNamedQuery("User.findByName", User.class);
+                do {
+                    System.out.print("UserName: ");
+                    createdUser.username = u.nextLine();
+                    q.setParameter("name", createdUser.username);
+                    User li = null;
+                    try {
+                        li = (User) q.getSingleResult();
+                    } catch (Exception ex) {
+                    }
+                    if (li == null) {
+                        break;
+                    }
+                    System.out.println("The name already exist");
+                } while (true);
+            } else if (tempS.toLowerCase().contains("cridet")) {
+                System.out.print("cridet Card Number: ");
+                String t = u.nextLine();
+                signedInUser.setCridetCard(t);
+            } else if (tempS.toLowerCase().contains("token") || tempS.toLowerCase().contains("id")) {
+                System.out.print("Not allowed action");
+                return;
+            }
+            manager.merge(signedInUser);
+            transaction.commit();
+        } else {
+            System.out.println(token);
+        }
+    }
+
     static public void Command(String command) throws InterruptedException {
         Scanner u = new Scanner(System.in);
-        if (command.contains("q:ch")) {
-            System.out.println("Please sign in first: ");
-            System.out.print("Enter UserName: ");
-            String username = u.nextLine();
-            System.out.print("Enter Password: ");
-            String password = u.nextLine();
-            var signINUser = new UserInput(username, password);
-            var token = SignIn(signINUser, null);
-            if (!token.contains("Error::")) {
-                User signedInUser = users.get(token).currentUser;
-                System.out.println("DETAILS :: " + signedInUser);
-                System.out.print("Enter what you data you change: ");
-                String tempS = u.nextLine();
-                transaction.begin();
-
-                if (tempS.toLowerCase().contains("email")) {
-                    System.out.print("Email: ");
-                    String t = u.nextLine();
-                    signedInUser.setEmail(t);
-                } else if (tempS.toLowerCase().contains("password")) {
-                    System.out.print("password: ");
-                    String t = u.nextLine();
-                    signedInUser.setPassword(t);
-                } else if (tempS.toLowerCase().contains("name")) {
-                    createUser createdUser = new createUser();
-                    Query q = manager.createNamedQuery("User.findByName", User.class);
-                    do {
-                        System.out.print("UserName: ");
-                        createdUser.username = u.nextLine();
-                        q.setParameter("name", createdUser.username);
-                        User li = null;
-                        try {
-                            li = (User) q.getSingleResult();
-                        } catch (Exception ex) {
-                        }
-                        if (li == null) {
-                            break;
-                        }
-                        System.out.println("The name already exist");
-                    } while (true);
-                } else if (tempS.toLowerCase().contains("cridet")) {
-                    System.out.print("cridet Card Number: ");
-                    String t = u.nextLine();
-                    signedInUser.setCridetCard(t);
-                } else if (tempS.toLowerCase().contains("token") || tempS.toLowerCase().contains("id")) {
-                    System.out.print("Not allowed action");
-                    return;
-                }
-                manager.merge(signedInUser);
-                transaction.commit();
-            } else {
-                System.out.println(token);
-            }
-        } else if (command.contains(
-                "q:up")) {
+        if (command.contains("ch")) {
+            changeData();
+        } else if (command.contains("up")) {
             System.out.println("Please Enter the following data:");
             createUser createdUser = new createUser();
             Query q = manager.createNamedQuery("User.findByName", User.class);
@@ -216,26 +234,27 @@ public class InformantDB {
             createdUser.creditCard = u.nextLine();
             createUser(createdUser);
         } else if (command.toLowerCase().contains(
-                "q:tk")) {
+                "tk")) {
             System.out.print("Enter Token: ");
-            SignIn(null, u.nextLine());
+            var tokenOrError = tokenSignIn(u.nextLine());
+            if (!tokenOrError.contains("Error")) {
+                AfterSignIn(tokenOrError);
+            }
 
-        } else if (command.toLowerCase().contains(
-                "q:in")) {
+        } else if (command.toLowerCase().contains("in")) {
 
             System.out.print("Enter UserName: ");
             String username = u.nextLine();
             System.out.print("Enter Password: ");
             String password = u.nextLine();
             var signINUser = new UserInput(username, password);
-            var tokenOrError = SignIn(signINUser, null);
-            if(!tokenOrError.contains("Error")){
+            var tokenOrError = SignIn(signINUser);
+            if (!tokenOrError.contains("Error")) {
                 AfterSignIn(tokenOrError);
             }
         }
     }
 
-    ///omar
     static public String insert(IAd i, User uu) {
         Ads ad = new Ads();
         ad.setStars(i.getStars());
@@ -248,7 +267,9 @@ public class InformantDB {
                 q.setParameter("id", i.getImageId());
                 Images ii = (Images) q.getSingleResult();
                 ad.setImageId(ii);
-            }else{ad.setImageId(null);}
+            } else {
+                ad.setImageId(null);
+            }
             q = manager.createNamedQuery("Payment.findById", Payment.class);
             q.setParameter("id", i.getPaymentId());
             Payment pp = (Payment) q.getSingleResult();
@@ -266,22 +287,43 @@ public class InformantDB {
             return "Error::" + ex.getMessage();
         }
     }
-    ///omar
+
+    static public void signout(String Token) {
+        if (users.containsKey(Token)) {
+            users.remove(Token);
+            System.out.println("Signout succeeded!");
+            return;
+        } else {
+            System.out.println("User not signed in");
+            AfterSignIn(Token);
+        }
+    }
 
     static public void AfterSignIn(String Token) {
-        System.out.print("Enter Query:\nq:create - create new ad: ");
-        Scanner u = new Scanner(System.in);
-        u.nextLine();
-        IAd iad = new IAd();
-        System.out.print("set Price: ");
-        iad.setPrice(u.nextFloat());
-        u.nextLine();
-        System.out.print("insert ad Details: ");
-        iad.setDetails(u.nextLine());
-        iad.setImageId(-1L);
-        iad.setPaymentId(3);
-        var mes = insert(iad,users.get(Token).currentUser);
-        System.out.println(mes);
+        var currentUser = users.get(Token).currentUser;
+        do {
+
+            System.out.println("Welcome " + currentUser.getName() + " 😎. What you like to do?\nEnter any Query you want\nq:crt - create new ad");
+            System.out.println("q:out    - signing out user");
+            System.out.print("%q:");
+            Scanner u = new Scanner(System.in);
+            String command = u.nextLine();
+            if (command.toLowerCase().contains("out")) {
+                signout(Token);
+                return;
+            } else if (command.toLowerCase().contains("crt")) {
+                IAd iad = new IAd();
+                System.out.print("set Price: ");
+                iad.setPrice(u.nextFloat());
+                u.nextLine();
+                System.out.print("insert ad Details: ");
+                iad.setDetails(u.nextLine());
+                iad.setImageId(-1L);
+                iad.setPaymentId(3);
+                var mes = insert(iad, users.get(Token).currentUser);
+                System.out.println(mes);
+            }
+        } while (true);
     }
 
     /**
@@ -299,14 +341,13 @@ public class InformantDB {
             System.out.println("q:ch - change user data ");
             System.out.println("q:li - list available ads");
             System.out.println("q:E - Exit ");
-            System.out.print("Enter Command (\"don't forget ((q:)) before the query\"): ");
+            System.out.print("%q:");
             Scanner c = new Scanner(System.in);
             String ss = c.nextLine();
             Command(ss);
-            if (ss.toLowerCase().contains("q:li")) {
+            if (ss.toLowerCase().contains("li")) {
                 System.out.println(listAds());
-            }
-            else if (ss.toLowerCase().contains("q:e")) {
+            } else if (ss.toLowerCase().contains("e")) {
                 break;
             }
         } while (true);
